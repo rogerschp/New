0cc51c8a940c42b56b4de0c8ce3d7be949ba701c
rogerschp <52896404+rogerschp@users.noreply.github.com>
2019-12-23 15:38:29 -0300

    NotasNaoLancadas

---
+++ NotasNaoLancadas_Speed_Controle/NotasNaoLancadas_Speed_Controle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.WaitHelpers;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace NotasNaoLancadas_Speed_Controle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var option = new ChromeOptions();
+            option.AddExtension(@"C:\Users\Roger\Downloads\extensao (1).crx");
+            IWebDriver driver = new ChromeDriver(option);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.fsist.com.br/");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(7200));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("chave")));
+            driver.FindElement(By.Id("chave")).SendKeys("35190183310441007553550020000166921682089800");
+            SendKeys.SendWait("{Enter}");
+
+        }
+    }
+}
